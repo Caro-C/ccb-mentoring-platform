11f9e36075ff4a63ca97a399c86276cfbd8b01ba
Carolyn <carolyn.s.covic@gmail.com>
2020-10-11 17:20:53 +0200

    add filter function

--- js/main.js
+++ js/main.js
@@ -61,3 +61,48 @@ sr.reveal('.animate-center',{
   distance: '25rem',
   delay: 200
 });
+
+// Filters for mentors page
+
+$(document).ready(function() {
+
+  let $grid = $('.our-mentors-grid');
+  let $items = $grid.children('.mentor-tile');
+  let currentCat = 'all';
+  let sidebar = [];
+  let sortItems = (a,b) => {
+    let an = a.getAttribute('data-order');
+    let bn = b.getAttribute('data-order');
+
+    if(an > bn) { return 1; }
+    if(an < bn) { return -1; }
+    return 0;
+  }
+
+  let filterItems = function() {
+    let cat = this.getAttribute('data-category');
+    let newSidebar = [];
+
+    $('.button--is-active').toggleClass('button--is-active');
+    $(`.filter-button[data-category=${cat}]`).toggleClass('button--is-active');
+
+    $grid.fadeOut( "slow", function() {
+
+      sidebar.map((item) => $(item).appendTo($grid));
+
+      if(cat === 'all') {
+        $items.sort(sortItems).detach().appendTo($grid);
+      } else {
+        $(`.mentor-tile:not([data-category=${cat}])`).each(function() {
+          newSidebar.push($(this).detach());
+        });
+      }
+      sidebar = newSidebar;
+      currentCat = cat;
+    }).fadeIn("slow");
+  };
+
+  // Handle the click on a category filter button
+  $('.js-button-filter').click(filterItems);
+
+});
